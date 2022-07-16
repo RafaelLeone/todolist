bbb3490a2260d430073f64058a687b24a396a32b
RafaelLeone <rinaciodacosta@gmail.com>
2022-07-16 18:28:03 -0300

    static root

--- todolist/settings.py
+++ todolist/settings.py
@@ -71,7 +71,9 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        
     },
+    
 ]
 
 WSGI_APPLICATION = 'todolist.wsgi.application'
@@ -127,7 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'tasks/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
